4e4a93b29b3926ee77e7fc3426944d09602b69ef
fragnarxx <fabio25.rodriguez@gmail.com>
2021-03-12 10:20:28 +0100

    reading .mat files

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+import os
+import scipy.io
+import sys
+
 from classification import sd_classify
 
 
 if __name__ == "__main__":
 
+    dirname = os.path.dirname(__file__)
     
     # Grupo 1, Caudal ascendente
     
-    root_path = "./data/Recirculación/Grupo_1_170809/Caudal_ascendente"
+    relative_path = 'data/Recirculación/Grupo_1_170809/Caudal_ascendente/'
+    no_fail_path = 'Datos_sin_fallo/G1_sin_fallos.mat'
+    neg_offset_path = '1_Offset positivo en caudal/G1_con_fallos.mat'
+    pos_offset_path = '2_Offset negativo en caudal/G1_con_fallo2.mat'
 
-    with open(root_path + "/Datos_sin_fallo/G1_sin_fallos_dcr.mat", "r") as file:
-        print(file.read())
+    G1_good = scipy.io.loadmat(os.path.join(dirname, relative_path+no_fail_path))
 
-    G1_good = None
+    G1_positive_offset = scipy.io.loadmat(os.path.join(dirname, relative_path+pos_offset_path))
 
-    G1_positive_offset = None
-
-    G1_negative_offset = None
+    G1_negative_offset = scipy.io.loadmat(os.path.join(dirname, relative_path+neg_offset_path))
     
+    print(G1_good)
+    print(G1_positive_offset)
+    print(G1_negative_offset)
